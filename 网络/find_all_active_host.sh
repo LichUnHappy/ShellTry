594d97e55dbe44fb6d63464bde7b83efcf14608e
bullgo <brand_daijh@163.com>
2018-08-19 13:15:51 +0800

    Sharp in Net

--- 网络/find_all_active_host.sh
+++ 网络/find_all_active_host.sh
@@ -1,13 +1,37 @@
-#! /bin/bash
-# 文件名： ping.sh
+# #! /bin/bash
+# # 文件名： ping.sh
 
-for ip in 192.168.0.{1..255};
-do
-    ping $ip -c 2 &> /dev/null;
+# for ip in 192.168.0.{1..255}
+# do
+#     ping $ip -c 2 &> /dev/null;
 
-    if [ &? -eq 0 ]
-    then 
-        echo $ip is alive
-    fi
+#     if [ $? -eq 0 ]
+#     then 
+#         echo $ip is alive
+#         break
+#     fi
+# done
+
+for ip in `seq 1 255`
+
+  do
+
+    {
+
+       ping -c 1 192.168.0.$ip > /dev/null 2>&1
+
+       if [ $? -eq 0 ]; then
+
+          echo   192.168.0.$ip UP
+
+       else
+
+          echo   192.168.0.$ip DOWN
+
+       fi
+
+     }&
+
 done
 
+wait
