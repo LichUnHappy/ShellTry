4dc4f3331efcac8ab8bea657cacf63d5f2a00afa
bullgo <brand_daijh@163.com>
2018-08-21 13:10:00 +0800

    Fly with Text

--- 文本/dash_or_bash.sh
+++ 文本/dash_or_bash.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 echo -e "line1 f2 f3\nline2 f4 f5\nline3 f6 f7" |\ 
 awk '{
-    print "NR","NF","$0"=$0,"$1"=$1,"$2"=$2,"$3"=$3;
+    print $NR, $NF, $0, $1, $2, $3;
 }'
 
 # Still bug
+# CLEAR 
